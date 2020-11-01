e7ce0b12652e3ad3ccb437cb635c1a24d545ca18
michbelle <mi94bell@gmail.com>
2020-11-01 21:39:47 +0100

    text

---
+++ matlab_working_with_espcam/find_cicle_example.m
@@ -0,0 +1,18 @@
+
+cam = ipcam('http://192.168.1.6/')
+preview(cam) %live stream of the camera
+
+%code finding circle
+picture = snapshot(camera);
+picture = imresize(picture,[250,250]);
+image(picture);
+
+d = imdistline;
+
+delete(d)
+
+[centers,radii] = imfindcircles(picture,[10 20]);
+h = viscircles(centers,radii);
+
+[centers,radii] = imfindcircles(picture,[10 20],'Sensitivity',0.9);
+h = viscircles(centers,radii);
